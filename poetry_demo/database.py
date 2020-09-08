6cc248034af47d59fc195a9d8f39fd1526c80ff9
pk karki <prabin@abhiyantrik.com>
2020-09-08 09:26:21 +0545

    postgresSQl added localhost

--- poetry_demo/database.py
+++ poetry_demo/database.py
@@ -1,14 +1,18 @@
 import databases
 import sqlalchemy
 # SQLAlchemy specific code, as with any other app
-DATABASE_URL = "sqlite:///./test.db"
-# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "postgresql://prabin:prabin@localhost/project_db"
 
 
 database = databases.Database(DATABASE_URL)
 
+# engine = sqlalchemy.create_engine(
+#     DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
 engine = sqlalchemy.create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 
